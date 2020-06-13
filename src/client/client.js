a1bd2e29be11be9db4a680e8a3e9e8cc1207fec7
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-13 20:59:40 +0530

    added routing

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,12 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
---
+++ src/client/components/UsersList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UserList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map((user) => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Heres a big list of users: </h1>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
